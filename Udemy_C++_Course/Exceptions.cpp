e65b1d90e1176bd7e77bce90e4d8eb8b39d12053
James Scott <jg1816@hotmail.com>
2021-10-25 23:19:47 -0400

    I added a std::string exception

--- Udemy_C++_Course/Exceptions.cpp
+++ Udemy_C++_Course/Exceptions.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void mightGoWrong(){
 
 	bool error = true;
+	bool error2 = true;
+
 	if (error) {
 		throw "Something went wrong. ";
+	}
+
+	if (error2) {
+		throw string("Something else went wrong."); //Use the class name to create an object
 	}
 }
 
@@ -19,6 +26,9 @@ int main() {
 	}
 	catch(char  const * e){ //Catch primitive string char const * pointer errors
 		cout << "Error message: " << e << endl;
+	}
+	catch (string &e) { //multiple catch blocks - string class
+		cout << "string error message: " << e << endl;
 	}
 
 	cout << "Still running" << endl;
