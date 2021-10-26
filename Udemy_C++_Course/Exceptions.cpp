9a614423b87ae839b55aba16d307ce8b0eca6d3f
James Scott <jg1816@hotmail.com>
2021-10-25 23:12:09 -0400

    Add exception handling

--- Udemy_C++_Course/Exceptions.cpp
+++ Udemy_C++_Course/Exceptions.cpp
@@ -4,8 +4,23 @@ using namespace std;
 
 void mightGoWrong(){
 
+	bool error = true;
+	if (error) {
+		throw "Something went wrong. ";
+	}
 }
 
 int main() {
+	try {
+		mightGoWrong();
+	}
+	catch (int e) { //catch errors thrown of type int
+		cout << "Error code: " << e << endl;
+	}
+	catch(char  const * e){ //Catch primitive string char const * pointer errors
+		cout << "Error message: " << e << endl;
+	}
+
+	cout << "Still running" << endl;
 	return 0;
 }
